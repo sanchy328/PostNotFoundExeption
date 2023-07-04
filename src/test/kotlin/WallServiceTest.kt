d3f081b529b86112532ac8ddf07e27849c3b8c11
Sanchous <axanutin@mail.ru>
2023-07-04 21:24:55 +0300

    Test createComment

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -7,11 +7,20 @@ import ru.netology.WallService
 
 class WallServiceTest{
 
+    @Test
+    fun createCommentTest() {
+        WallService.add(Post(1))
+        val(id) = WallService.createComment(1, Comment())
+
+        val result = id
+
+        assertEquals(1, result)
+    }
+
     @Test(expected = PostNotFoundException::class)
     fun shouldThrow() {
         WallService.add(Post(1))
         WallService.add(Post(2))
-        WallService.createComment(3, Comment())
+        WallService.createComment(4, Comment())
     }
-
 }
